cdd47372728d338fa8320b72e18df7b255e2264f
Visal Munasinghe <127712545+vish-mv@users.noreply.github.com>
2024-09-18 16:52:08 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
 function App() {
+  // Access the environment variable
+  const testEnvVar = process.env.REACT_APP_TEST;
+
+  // Print the variable to the console
+  useEffect(() => {
+    console.log("Environment Variable TEST:", testEnvVar);
+  }, [testEnvVar]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{testEnvVar}</h1>
       </header>
     </div>
   );
